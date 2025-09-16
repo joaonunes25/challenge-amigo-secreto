71c29e545280f78de97bf1c53173637f6ee9f310
joaonunes25 <joaomn4@al.insper.edu.br>
2025-09-15 21:02:37 -0300

    aplicação finalizada

--- app.js
+++ app.js
@@ -1,1 +1,44 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let listaAmigos = [];
+
+function limparCampo() {
+    campo = document.getElementById("amigo");
+    campo.value = "";
+}
+
+function atualizaLista(listaAmigos) {
+    listaExibida = document.getElementById("listaAmigos");
+    listaExibida.innerHTML = "";
+
+    for (let amigo of listaAmigos) {
+        let item = document.createElement("li");
+        item.textContent = amigo;
+        listaExibida.appendChild(item);
+    }
+}
+
+function adicionarAmigo(listaAmigos) {
+    let amigo = document.querySelector('input').value;
+
+    if (amigo != "") {
+        listaAmigos.push(amigo);
+    } else {
+        alert("Por favor, insira um nome.");
+    }
+
+    limparCampo();
+
+    atualizaLista(listaAmigos);
+}
+
+function sortearAmigo(listaAmigos) {
+    let tamanhoLista = listaAmigos.length;
+    if (tamanhoLista > 0) {
+        let indiceSorteado = Math.floor(Math.random() * tamanhoLista);
+        let amigoSorteado = listaAmigos[indiceSorteado];
+
+        let resultado = document.getElementById("resultado");
+        resultado.innerHTML = `O amigo secreto sorteado é: ${amigoSorteado}`;
+    }
+
+}
